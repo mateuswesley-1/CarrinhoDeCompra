72dba247fe8570c9801c85de0ccf5e4ac948c5e7
Mateus Wesley <mateuswesley314@gmail.com>
2023-06-08 17:28:33 -0300

    mateus

---
+++ CarrinhoDeCompra/CarrinhoDeCompraDados.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace CarrinhoComprasAPI
+{
+    public class Item
+    {
+        public Item(string nome, decimal peso, decimal valor, string tipo)
+        {
+            Nome = nome;
+            Peso = peso;
+            Valor = valor;
+            Tipo = tipo;
+        }
+
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public decimal Peso { get; set; }
+        public string Tipo { get; set; }
+    }
+
+    public class CarrinhoCompras
+    {
+        private List<Item> itens;
+
+        public CarrinhoCompras()
+        {
+            itens = new List<Item>();
+        }
+
+        public void AdicionarItem(Item item)
+        {
+            itens.Add(item);
+        }
+
+        public int quantidadeItens()
+        {
+            return itens.Count;
+        }
+        public decimal calcularPesoTotal()
+        {
+            return itens.Sum(item => item.Peso);
+        }
+
+        public decimal CalcularFrete()
+        {
+            decimal pesoTotal = calcularPesoTotal();
+            int quantidade = quantidadeItens();
+            decimal frete = 0;
+            
+            if(quantidade >= 6)
+            {
+                frete += 10;
+            }
+
+            if (pesoTotal <= 2)
+            {
+                frete += 0;
+            }
+            else if (pesoTotal <= 10)
+            {
+               frete += (2.00m * pesoTotal);
+            }
+            else if (pesoTotal <= 50)
+            {
+                frete += (4.00m * pesoTotal);
+            }
+            else
+            {
+                frete += (7.00m * pesoTotal);
+            }
+
+            return frete;
+        }
+
+        public decimal CalcularDesconto()
+        {
+            decimal valorProdutos = CalcularValorProdutos();
+            decimal frete = CalcularFrete();
+            decimal desconto = 0;
+            Dictionary<string, int> tiposDic = new Dictionary<string, int>();
+
+
+            foreach (Item item in itens)
+            {
+                if (tiposDic.ContainsKey(item.Tipo))
+                {
+                    tiposDic[item.Tipo] += 1;
+                }
+                else
+                {
+                    tiposDic.Add(item.Tipo, 1);
+                }
+            }
+
+            if(tiposDic.Values.Any(valor => valor > 2))
+            {
+                desconto += frete * 0.05m;
+            }
+
+            if (valorProdutos > 500.00m && valorProdutos <= 1000.00m)
+            {
+                desconto += valorProdutos * 0.10m;
+            }
+            else if (valorProdutos > 1000.00m)
+            {
+                desconto += valorProdutos * 0.20m;
+            }
+
+            return desconto;
+        }
+
+        public decimal CalcularValorProdutos()
+        {
+            return itens.Sum(item => item.Valor);
+        }
+
+        public decimal CalcularValorTotal()
+        {
+            decimal valorTotal = CalcularValorProdutos();
+            decimal frete = CalcularFrete();
+            decimal desconto = CalcularDesconto();
+
+            return valorTotal + frete - desconto;
+        }
+    }
+
+    public class CarrinhoComprasService
+    {
+        private CarrinhoCompras carrinho;
+
+        public CarrinhoComprasService()
+        {
+            carrinho = new CarrinhoCompras();
+        }
+
+        public void AdicionarItem(Item item)
+        {
+            carrinho.AdicionarItem(item);
+        }
+
+        public decimal CalcularValorTotal()
+        {
+            return carrinho.CalcularValorTotal();
+        }
+    }
+
+    public class CarrinhoComprasController
+    {
+        private CarrinhoComprasService carrinhoService;
+
+        public CarrinhoComprasController()
+        {
+            carrinhoService = new CarrinhoComprasService();
+        }
+
+        public void AdicionarItem(string nome, decimal valor, decimal peso, string tipo)
+        {
+            var item = new Item (nome, valor, peso , tipo);
+            carrinhoService.AdicionarItem(item);
+        }
+
+        public decimal CalcularValorTotal()
+        {
+            return carrinhoService.CalcularValorTotal();
+        }
+    }
+}
+
---
+++ CarrinhoDeCompra/CarrinhoDeCompraProcessorTest.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+using System.IO;
+using Xunit;
+using CarrinhoComprasAPI;
+
+namespace CarrinhoDeCompra
+{
+    public class CarrinhoDeCompraProcessorTest
+    {
+        [Theory]
+        [MemberData(nameof(GetCarrinhoComprasTestCases))]
+        public void Should_Return_Valor_Total_Correto(List<Item> itens, decimal valorProduto, decimal pesoTotal, decimal valorFrete, decimal desconto, decimal valorTotal)
+        {
+            // Arrange
+            var carrinho = new CarrinhoCompras();
+
+            // Act
+            foreach (var item in itens)
+            {
+                carrinho.AdicionarItem(item);
+            }
+
+            var valorTotalCalculado = carrinho.CalcularValorTotal();
+            var valorFreteCalculado = carrinho.CalcularFrete();
+            var valorProdutoCalculado = carrinho.CalcularValorProdutos();
+            var pesoTotalCalculado = carrinho.calcularPesoTotal();
+
+            // Assert
+            Assert.Equal(valorTotal, valorTotalCalculado);
+
+        }
+
+        [Theory]
+        [MemberData(nameof(GetCarrinhoComprasTestCases))]
+        public void Should_Return_Frete_Correto(List<Item> itens, decimal valorProduto, decimal pesoTotal, decimal valorFrete, decimal desconto, decimal valorTotal)
+        {
+            // Arrange
+            var carrinho = new CarrinhoCompras();
+
+            // Act
+            foreach (var item in itens)
+            {
+                carrinho.AdicionarItem(item);
+            }
+
+            var valorFreteCalculado = carrinho.CalcularFrete();
+
+            // Assert
+            Assert.Equal(valorFrete, valorFreteCalculado);
+
+        }
+
+        [Theory]
+        [MemberData(nameof(GetCarrinhoComprasTestCases))]
+        public void Should_Return_Peso_Total_Correto(List<Item> itens, decimal valorProduto, decimal pesoTotal, decimal valorFrete, decimal desconto, decimal valorTotal)
+        {
+            // Arrange
+            var carrinho = new CarrinhoCompras();
+
+            // Act
+            foreach (var item in itens)
+            {
+                carrinho.AdicionarItem(item);
+            }
+
+            var pesoTotalCalculado = carrinho.calcularPesoTotal();
+
+            // Assert
+            Assert.Equal(pesoTotal, pesoTotalCalculado);
+
+        }
+
+        [Theory]
+        [MemberData(nameof(GetCarrinhoComprasTestCases))]
+        public void Should_Return_Valor_Produtos_Correto(List<Item> itens, decimal valorProduto, decimal pesoTotal, decimal valorFrete, decimal desconto, decimal valorTotal)
+        {
+            // Arrange
+            var carrinho = new CarrinhoCompras();
+
+            // Act
+            foreach (var item in itens)
+            {
+                carrinho.AdicionarItem(item);
+            }
+
+            var valorProdutoCalculado = carrinho.CalcularValorProdutos();
+
+
+            // Assert
+            Assert.Equal(valorProduto, valorProdutoCalculado);
+
+        }
+        [Theory]
+        [MemberData(nameof(GetCarrinhoComprasTestCases))]
+        public void Should_Return_Valor_Desconto_Correto(List<Item> itens, decimal valorProduto, decimal pesoTotal, decimal valorFrete, decimal desconto, decimal valorTotal)
+        {
+            // Arrange
+            var carrinho = new CarrinhoCompras();
+
+            // Act
+            foreach (var item in itens)
+            {
+                carrinho.AdicionarItem(item);
+            }
+
+            var valorDescotoCalculado = carrinho.CalcularDesconto();
+
+
+            // Assert
+            Assert.Equal(desconto, valorDescotoCalculado);
+
+        }
+        public static IEnumerable<object[]> GetCarrinhoComprasTestCases()
+        {
+            var testCases = ReadCsvFile("C:/Users/mateu/OneDrive - vrstd/Área de Trabalho/Residencia TCE/Estudos/.NET/CarrinhoDeCompra/carrinho.csv");
+            
+             return testCases.Select(testCase => new object[] { testCase.Itens, testCase.valorProduto, testCase.pesoTotal, testCase.valorFrete, testCase.desconto, testCase.valorTotal });
+        }
+
+        private static List<TestCase> ReadCsvFile(string filePath)
+        {
+            var testCases = new List<TestCase>();
+
+            using (var reader = new StreamReader(filePath))
+            {
+                reader.ReadLine();
+                string line;
+                TestCase currentTestCase = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("Caso"))
+                    {
+                        if (currentTestCase != null)
+                        {
+                            testCases.Add(currentTestCase);
+                        }
+
+                        currentTestCase = new TestCase();
+                        currentTestCase.Itens = new List<Item>();
+
+                        var values = line.Split(';');
+                        currentTestCase.Nome = values[0];
+                        currentTestCase.valorProduto = decimal.Parse(values[5]);
+                        currentTestCase.pesoTotal = decimal.Parse(values[6]);
+                        currentTestCase.valorFrete = decimal.Parse(values[7]);
+                        currentTestCase.desconto = decimal.Parse(values[8]);
+                        currentTestCase.valorTotal = decimal.Parse(values[9]);
+                    }
+                    if (currentTestCase != null)
+                    {
+                        var values = line.Split(';');
+
+                        var nome = values[1];
+                        var peso = decimal.Parse(values[2]);
+                        var valor = decimal.Parse(values[3]);
+                        var tipo = values[4];
+
+                        var item = new Item(nome, peso, valor, tipo);
+                        currentTestCase.Itens.Add(item);
+
+                    }
+                }
+                testCases.Add(currentTestCase);
+
+            }
+            return testCases;
+        }
+
+        public class TestCase
+        {
+            public string Nome { get; set; }
+            public List<Item> Itens { get; set; }
+
+            public decimal valorProduto { get; set; }
+            public decimal pesoTotal { get; set; }
+            public decimal valorFrete { get; set; }
+            public decimal desconto { get; set; }
+            public decimal valorTotal { get; set; }
+
+        }
+    }
+}
